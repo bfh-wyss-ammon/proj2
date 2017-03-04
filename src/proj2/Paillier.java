0506ff5db797743b593e014064d0f1bc51328631
wyssg2 <gw1@gmx.ch>
2017-03-04 18:51:52 +0100

    modulus, prime certainty and generator hardcoded

--- src/proj2/Paillier.java
+++ src/proj2/Paillier.java
@@ -1,103 +1,97 @@
 package proj2;
-import java.util.*;
+
 import java.math.*;
 import java.security.SecureRandom;
 
+/**
+ * @author gabe this class implements the homomorphic crypto scheme
+ */
 public class Paillier {
+	// sets the size of the key material
+	public final int modulus = 2048;
+	// sets how many rounds of the miller rabin test are run
+	public final int prime_certainty = 256;
 
-private BigInteger g;
-private int modulus;
+	
+	//the generator
+	private BigInteger g;
+	
+	private BigInteger p;
+	private BigInteger q;
+	
+	//the random generator used for cryptographic operations
+	private SecureRandom rand;
+	
+	private BigInteger lambda;
+	public BigInteger n;
+	public BigInteger nsquared;
 
-private BigInteger p;
-private BigInteger q;
-private SecureRandom rand = new SecureRandom();
-private BigInteger lambda;
-public BigInteger n;
-public BigInteger nsquared;
+	/**
+	 * create a new instance of this cryptosystem with new keys
+	 */
+	public Paillier() {
+		 rand= new SecureRandom();
+		try {
+			keyGen();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("There was an error in keygen");
+		}
 
+	}
 
-public Paillier(int modulus, int prime_certainty) {
-keyGen(modulus, prime_certainty);
-}
+	public void keyGen() throws Exception {
+		p = new BigInteger(this.modulus / 2, this.prime_certainty, this.rand);
+		q = new BigInteger(this.modulus / 2, this.prime_certainty, this.rand);
 
+		this.n = this.p.multiply(this.q);
+		this.nsquared = this.n.multiply(this.n);
 
-public Paillier() {
-keyGen(512, 64);
-}
+		this.g = new BigInteger("2");
+		lambda = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE))
+				.divide(p.subtract(BigInteger.ONE).gcd(q.subtract(BigInteger.ONE)));
 
-public void keyGen(int modulus, int prime_certainty) {
-p = new BigInteger(modulus / 2, prime_certainty, rand);
-q = new BigInteger(modulus / 2, prime_certainty, rand);
+		if (g.modPow(lambda, nsquared).subtract(BigInteger.ONE).divide(n).gcd(n).intValue() != 1) {
+			throw new Exception();
+		}
+	}
 
-n = p.multiply(q);
-nsquared = n.multiply(n);
-this.modulus=modulus;
-g = new BigInteger("2");
-lambda = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE)).divide(
-p.subtract(BigInteger.ONE).gcd(q.subtract(BigInteger.ONE)));
+	public BigInteger Encryption(BigInteger m, BigInteger r) {
+		System.out.println("g pot: " + g.modPow(m, nsquared));
+		System.out.println("r pot: " + r.modPow(n, nsquared));
 
-if (g.modPow(lambda, nsquared).subtract(BigInteger.ONE).divide(n).gcd(n).intValue() != 1) {
-System.out.println("g is not good. Choose g again.");
-System.exit(1);
-}
-System.out.println("p: " + p);
-System.out.println("q: " + q);
-System.out.println("n: " + n);
-System.out.println("nsquared " + nsquared);
-System.out.println("lambda: " + lambda);
-}
+		return g.modPow(m, nsquared).multiply(r.modPow(n, nsquared)).mod(nsquared);
+	}
 
+	public BigInteger Encryption(BigInteger m) {
+		BigInteger r = new BigInteger(modulus, rand);
+		return g.modPow(m, nsquared).multiply(r.modPow(n, nsquared)).mod(nsquared);
 
-public BigInteger Encryption(BigInteger m, BigInteger r) {
-	System.out.println("g pot: " + g.modPow(m, nsquared));
-	System.out.println("r pot: " + r.modPow(n, nsquared));
+	}
 
-return g.modPow(m, nsquared).multiply(r.modPow(n, nsquared)).mod(nsquared);
-}
+	public BigInteger Decryption(BigInteger c) {
+		BigInteger u = g.modPow(lambda, nsquared).subtract(BigInteger.ONE).divide(n).modInverse(n);
+		return c.modPow(lambda, nsquared).subtract(BigInteger.ONE).divide(n).multiply(u).mod(n);
+	}
 
+	public static void main(String[] str) {
+		Paillier paillier = new Paillier();
+		BigInteger m1 = new BigInteger("20000000");
+		BigInteger m2 = new BigInteger("60");
 
-public BigInteger Encryption(BigInteger m) {
-BigInteger r = new BigInteger(modulus, rand);
+		BigInteger c1 = paillier.Encryption(m1);
+		BigInteger c2 = paillier.Encryption(m2);
 
-System.out.println("g pot: " + g.modPow(m, nsquared));
-System.out.println("r pot: " + r.modPow(n, nsquared));
-System.out.println("r: " + r);
+		System.out.println("Ciphertext c1: " + c1.intValue());
+		System.out.println("Ciphertext c2: " + c2.intValue());
 
-return g.modPow(m, nsquared).multiply(r.modPow(n, nsquared)).mod(nsquared);
+		System.out.println("Decrypted c1: " + paillier.Decryption(c1).intValue());
+		System.out.println("Decrypted c2: " + paillier.Decryption(c2).intValue());
 
-}
+		BigInteger product_em1em2 = c1.multiply(c2).mod(paillier.nsquared);
+		BigInteger sum_m1m2 = m1.add(m2).mod(paillier.n);
+		System.out.println("original sum: " + sum_m1m2.toString());
+		System.out.println("decrypted sum: " + paillier.Decryption(product_em1em2).toString());
 
-
-public BigInteger Decryption(BigInteger c) {
-BigInteger u = g.modPow(lambda, nsquared).subtract(BigInteger.ONE).divide(n).modInverse(n);
-return c.modPow(lambda, nsquared).subtract(BigInteger.ONE).divide(n).multiply(u).mod(n);
-}
-
-
-public static void main(String[] str) {
-Paillier paillier = new Paillier();
-BigInteger m1 = new BigInteger("20");
-BigInteger m2 = new BigInteger("60");
-
-BigInteger c1 = paillier.Encryption(m1);
-BigInteger c2 = paillier.Encryption(m2);
-
-
-System.out.println("Ciphertext c1: " + c1.intValue() );
-System.out.println("Ciphertext c2: " + c2.intValue() );
-
-System.out.println("Decrypted c1: " + paillier.Decryption(c1).intValue() );
-System.out.println("Decrypted c2: " + paillier.Decryption(c2).intValue() );
-
-
-
-BigInteger product_em1em2 = c1.multiply(c2).mod(paillier.nsquared);
-BigInteger sum_m1m2 = m1.add(m2).mod(paillier.n);
-System.out.println("original sum: " + sum_m1m2.toString());
-System.out.println("decrypted sum: " + paillier.Decryption(product_em1em2).toString());
-
-
-
-
-}
+	}
 }
