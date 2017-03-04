d148276daebb38136770bb142fdd8acd20bf955c
wyssg2 <gw1@gmx.ch>
2017-03-04 19:40:22 +0100

    add the homo adding method

--- src/proj2/Paillier.java
+++ src/proj2/Paillier.java
@@ -4,27 +4,31 @@ import java.math.*;
 import java.security.SecureRandom;
 
 /**
- * @author gabe this class implements the homomorphic crypto scheme
+ * @author gabe 
+ * this class implements the homomorphic crypto scheme
  */
 public class Paillier {
 	// sets the size of the key material
 	public final int modulus = 2048;
+	
 	// sets how many rounds of the miller rabin test are run
 	public final int prime_certainty = 256;
 
 	
-	//the generator
+	//the generator (is equal to n+1)
 	private BigInteger g;
 	
+	//the public key
+	private BigInteger n;
+	private BigInteger nsquared;
 	private BigInteger p;
 	private BigInteger q;
 	
 	//the random generator used for cryptographic operations
 	private SecureRandom rand;
 	
+	//the private key
 	private BigInteger lambda;
-	public BigInteger n;
-	public BigInteger nsquared;
 
 	/**
 	 * create a new instance of this cryptosystem with new keys
@@ -41,57 +45,67 @@ public class Paillier {
 	}
 
 	public void keyGen() throws Exception {
+		
+		//find the public key
 		p = new BigInteger(this.modulus / 2, this.prime_certainty, this.rand);
 		q = new BigInteger(this.modulus / 2, this.prime_certainty, this.rand);
-
 		this.n = this.p.multiply(this.q);
 		this.nsquared = this.n.multiply(this.n);
-
-		this.g = new BigInteger("2");
-		lambda = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE))
-				.divide(p.subtract(BigInteger.ONE).gcd(q.subtract(BigInteger.ONE)));
-
-		if (g.modPow(lambda, nsquared).subtract(BigInteger.ONE).divide(n).gcd(n).intValue() != 1) {
+		this.g= this.n.add(BigInteger.ONE);
+		
+		//find and check the secret key
+		this.lambda = this.p.subtract(BigInteger.ONE).multiply(this.q.subtract(BigInteger.ONE))
+				.divide(this.p.subtract(BigInteger.ONE).gcd(this.q.subtract(BigInteger.ONE)));
+		if (this.g.modPow(this.lambda, this.nsquared).subtract(BigInteger.ONE).divide(this.n).gcd(this.n).intValue() != 1) {
 			throw new Exception();
 		}
 	}
 
-	public BigInteger Encryption(BigInteger m, BigInteger r) {
-		System.out.println("g pot: " + g.modPow(m, nsquared));
-		System.out.println("r pot: " + r.modPow(n, nsquared));
-
-		return g.modPow(m, nsquared).multiply(r.modPow(n, nsquared)).mod(nsquared);
-	}
-
-	public BigInteger Encryption(BigInteger m) {
-		BigInteger r = new BigInteger(modulus, rand);
-		return g.modPow(m, nsquared).multiply(r.modPow(n, nsquared)).mod(nsquared);
+	/**
+	 * @param m takes BigInt m as message
+	 * @return returns the encrypted ciphertext of m
+	 */
+	public BigInteger encryption(BigInteger m) {
+		BigInteger r = new BigInteger(this.modulus, this.rand);
+		return this.g.modPow(m, this.nsquared).multiply(r.modPow(this.n, this.nsquared)).mod(this.nsquared);
 
 	}
 
-	public BigInteger Decryption(BigInteger c) {
+	/**
+	 * @param c takes BigInt c as ciphertext
+	 * @return returns the decrypted cleartext of c
+	 */
+	public BigInteger decryption(BigInteger c) {
 		BigInteger u = g.modPow(lambda, nsquared).subtract(BigInteger.ONE).divide(n).modInverse(n);
 		return c.modPow(lambda, nsquared).subtract(BigInteger.ONE).divide(n).multiply(u).mod(n);
+	}
+	
+	/**
+	 * @param c1 ciphertext to be added
+	 * @param c2 ciphertext to be added 
+	 * @param n the public key
+	 * @return the encrypted result of the addition
+	 */
+	 public BigInteger homomorphicAdd(BigInteger c1, BigInteger c2){
+		return c1.multiply(c2).mod(this.nsquared);
 	}
 
 	public static void main(String[] str) {
 		Paillier paillier = new Paillier();
-		BigInteger m1 = new BigInteger("20000000");
+		BigInteger m1 = new BigInteger("200");
 		BigInteger m2 = new BigInteger("60");
 
-		BigInteger c1 = paillier.Encryption(m1);
-		BigInteger c2 = paillier.Encryption(m2);
+		BigInteger c1 = paillier.encryption(m1);
+		BigInteger c2 = paillier.encryption(m2);
 
 		System.out.println("Ciphertext c1: " + c1.intValue());
 		System.out.println("Ciphertext c2: " + c2.intValue());
 
-		System.out.println("Decrypted c1: " + paillier.Decryption(c1).intValue());
-		System.out.println("Decrypted c2: " + paillier.Decryption(c2).intValue());
+		System.out.println("Decrypted c1: " + paillier.decryption(c1).intValue());
+		System.out.println("Decrypted c2: " + paillier.decryption(c2).intValue());
 
-		BigInteger product_em1em2 = c1.multiply(c2).mod(paillier.nsquared);
-		BigInteger sum_m1m2 = m1.add(m2).mod(paillier.n);
-		System.out.println("original sum: " + sum_m1m2.toString());
-		System.out.println("decrypted sum: " + paillier.Decryption(product_em1em2).toString());
+		System.out.println("original sum: " + m1.add(m2).intValue());
+		System.out.println("decrypted sum: " + paillier.decryption(paillier.homomorphicAdd(c1,c2)).intValue());
 
 	}
 }
