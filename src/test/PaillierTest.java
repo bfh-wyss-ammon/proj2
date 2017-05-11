b540f7572c6d9638a5eb83d05f1111d7b46c0414
ammop2 <pascal.ammon@gmail.com>
2017-05-11 21:23:38 +0200

    create test-cases for pailler

--- src/test/PaillierTest.java
+++ src/test/PaillierTest.java
@@ -4,24 +4,38 @@ import static org.junit.Assert.*;
 
 import java.math.BigInteger;
 
+import org.junit.Before;
 import org.junit.Test;
 import src.proj2.*;
 
 public class PaillierTest {
 	
+	private BigInteger v1;
+	private BigInteger v2;
+	private BigInteger v1e;
+	private BigInteger v2e;
+	
+	Paillier providerPailler;
+	
+	@Before
+	public void Setup() {		
+		providerPailler = new Paillier();
+		v1 = BigInteger.valueOf(5);
+		v2 = BigInteger.valueOf(205);
+		v1e = providerPailler.encryption(v1);
+		v2e = providerPailler.encryption(v2);
+		
+	}
+	
 	@Test
-	public void EqualTest() {
-		BigInteger i = BigInteger.valueOf(5);
-		
-		Paillier pailler = new Paillier();
-		BigInteger encodedValue = pailler.encryption(i);
-		BigInteger decodedValue = pailler.decryption(encodedValue);
-		
-		System.out.println("check value: " + i);
-		System.out.println("encoded value: " + encodedValue);
-		System.out.println("decoded value: " + decodedValue);
-		
-		assertEquals(i, decodedValue);
+	public void DecryptionTest() {				
+		assertEquals(v1, providerPailler.decryption(v1e));
+	}
+	
+	@Test
+	public void MobileTest() {
+		Paillier pailler = new Paillier(providerPailler.getPublicKey(), null);				
+		assertEquals(providerPailler.homomorphicAdd(v1e, v2e), pailler.homomorphicAdd(v1e, v2e));
 	}
 
 }
